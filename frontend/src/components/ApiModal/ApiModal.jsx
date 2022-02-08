ac538f80375672da006a6a3aa24c274b39bb3024
Aayush Jain <imjainaayush@gmail.com>
2022-02-08 11:47:28 +0530

    feat: added ui component for managing api

---
+++ frontend/src/components/ApiModal/ApiModal.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import styles from "./ApiModal.module.scss";
+import { postData } from "../../utils/fetchData";
+
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+
+const ApiModal = () => {
+  const navigate = useNavigate();
+  const [apiName, setApiName] = useState("");
+  const [apiEndpoint, setApiEndpoint] = useState("");
+  const [apiDescription, setApiDescription] = useState("");
+
+  const handleOnSubmit = (e) => {};
+
+  return (
+    <div className={styles.apiModal}>
+      <ToastContainer />
+      <div className={styles.h1}>Add New API</div>
+      <input
+        className={styles.inputField}
+        type="text"
+        placeholder="API Name"
+        value={apiName}
+        onChange={(e) => setApiName(e.target.value)}
+      />
+      <input
+        className={styles.inputField}
+        type="text"
+        placeholder="API End Point"
+        value={apiEndpoint}
+        onChange={(e) => setApiEndpoint(e.target.value)}
+      />
+      <input
+        className={styles.inputField}
+        type="text"
+        style={{ height: "100px" }}
+        placeholder="API Description"
+        value={apiDescription}
+        onChange={(e) => setApiDescription(e.target.value)}
+      />
+      <div className={styles.submitBtn} onClick={handleOnSubmit}>
+        Add API
+      </div>
+    </div>
+  );
+};
+
+export default ApiModal;
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,21 +1,44 @@
-import React from "react";
-import styles from "./Navbar.module.scss";
+import React, { useRef } from "react";
 import { useNavigate } from "react-router-dom";
+import SkyLight from "react-skylight";
 
+import APIModal from "../ApiModal/ApiModal";
+import styles from "./Navbar.module.scss";
 import Logo from "../Logo/Logo";
 
 const Header = ({ auth }) => {
   const navigate = useNavigate();
+  const newApiModal = useRef();
+
+  const modalStyle = {
+    position: "absolute",
+    width: "498px",
+    height: "515px",
+    left: "595px",
+    top: "140px",
+    margin: "0 auto",
+    border: "1px solid #dfe0e0",
+    boxSizing: "border-box",
+    borderRadius: "10px",
+  };
+
   return (
     <header className={styles.navbar}>
       <Logo />
+      <SkyLight
+        dialogStyles={modalStyle}
+        hideOnOverlayClicked
+        ref={newApiModal}
+      >
+        <APIModal />
+      </SkyLight>
       {auth ? (
         <div className={styles.rightNav}>
           <div className={styles.rightNavItem}>My APIs</div>
           <div className={styles.rightNavItem}>My Account</div>
           <div
             className={styles.rightNavBtn}
-            onClick={() => navigate("/login")}
+            onClick={() => newApiModal.current.show()}
           >
             + New API
           </div>
